2e2067ac87de91190207999456a9d8d6a09a26d9
ABDALQADER Ajil <ajil.abdalqader@soprasteria.com>
2022-04-29 11:17:20 +0200

    adding routing- only in XML

--- webapp/App.controller.js
+++ webapp/App.controller.js
@@ -7,21 +7,21 @@ sap.ui.define([
 ], function(Controller, JSONModel, Device, MessageToast, model) {
 	"use strict";
 
+
 	return Controller.extend("GeoMapSpots.App", {
 		onInit: function () {
 			
-			//var GeoMapData = model.createMyJSONModel("models/mockdata/data.json");
-         	//this.getView().setModel(GeoMapData);
-
-
-			this.oModel = new sap.ui.model.json.JSONModel("models/mockdata/data.json");
-			// this.oModel.setData(MapData);
+			
+			this.oModel = model.createMyJSONModel("models/mockdata/data.json");
+			
 			this.getView().setModel(this.oModel, "ViewModel");
-			// set the device model
+		
 			var oDeviceModel = new JSONModel(Device);
 			oDeviceModel.setDefaultBindingMode("OneWay");
 			this.getView().setModel(oDeviceModel, "device");
+
 		},
+
 
 		onPressLegend: function () {
 			if (this.byId("vbi").getLegendVisible() == true) {
@@ -36,9 +36,9 @@ sap.ui.define([
 		onPressResize: function () {
 			if (this.byId("btnResize").getTooltip() == "Minimize") {
 				if (Device.system.phone) {
-					this.byId("vbi").minimize(132, 56, 1320, 560);//Height: 3,5 rem; Width: 8,25 rem
+					this.byId("vbi").minimize(200, 150, 1320, 560);//Height: 3,5 rem; Width: 8,25 rem
 				} else {
-					this.byId("vbi").minimize(168, 72, 1680, 720);//Height: 4,5 rem; Width: 10,5 rem
+					this.byId("vbi").minimize(300, 200, 1680, 720);//Height: 4,5 rem; Width: 10,5 rem
 				}
 				this.byId("btnResize").setTooltip("Maximize");
 			} else {
@@ -49,21 +49,21 @@ sap.ui.define([
 
 		onLegendItemClick: function ( e )
 		{
-			// sap.m.MessageToast.show( "onLegendItemClick; clicked on  " + e.getParameters().id  );
 			//var oModel = this.getView().getModel();
 			var sLegendRegionText = this.oModel.getProperty("/LegendItems/" + e.getParameter("row") + "/text");
 			//var sLegendRegionText = this.oModel.getProperty("/LegendItems/" + e.getParameter("row")).text;
-
+			
 			//var aRegionProperties = this.oModel.getData().regionProperties;
 			var aRegionProperties = this.oModel.getProperty("/regionProperties");
-			
-			for ( var i = 0; i < aRegionProperties.length; ++i )
-			{
+			for ( var i = 0; i < aRegionProperties.length; ++i )	{
+				
 				var sTextBindingPath = "/regionProperties/" + i + "/text";
 				var sSelBindingPath = "/regionProperties/" + i + "/select";
 				var sRegtionText = this.oModel.getProperty( sTextBindingPath ); 
+				console.log( "onLegendItemClick; clicked on " + e.getParameters().i);
 				//var row = e.oSource.getText();
 				if ( sRegtionText === sLegendRegionText ){
+					
 					this.oModel.setProperty( sSelBindingPath, true );
 				}
 				else{
@@ -72,7 +72,25 @@ sap.ui.define([
 			}
 		},
 
+	// 	onLegendItemClick: function( e )
+	// {
+	// 	// this.getView().getModel(this.oModel);
+	// 	sap.m.MessageToast.show( "onLegendItemClick; clicked on  " + e.getParameters().id  );
 		
+	// 	for ( var nJ = 0; nJ < this.oModel.oData.regionProperties.length; ++nJ )
+	// 	{
+	// 	   var txt = "/regionProperties/" + nJ + "/text";
+	// 	   var sel = "/regionProperties/" + nJ + "/select";
+	// 	   var test = this.oModel.getProperty( txt ); 
+	// 	   var row = this.oModel.getProperty( sel ); 	
+	// 	   if ( test == row ){
+	// 		   this.oModel.setProperty( sel, true );
+	// 	   }
+	// 	   else{
+	// 		   this.oModel.setProperty( sel, false );
+	// 	   }
+	// 	}
+	// },
 		onDropItem: function (evt) {
 			MessageToast.show( "Item dropped!!" );
 		},
@@ -81,7 +99,6 @@ sap.ui.define([
 			evt.getSource().openDetailWindow("My Detail Window");
 		},
 
-	
 		onCloseDetail: function (evt) {
 			MessageToast.show("onCloseDetail" + this);
 		},
